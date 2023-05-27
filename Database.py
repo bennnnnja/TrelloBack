2788e64bb426f99fb221f4e534a916485013beaa
Kim Alexander <kim.aeduard@students.dvfu.ru>
2023-05-27 21:24:45 +1000

    added main, database, user

--- Database.py
+++ Database.py
@@ -1,6 +1,61 @@
-class my_class(object):
-    pass
+import user
 
+class Database: 
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.db_file = open(db_name, "a+")
+
+    def write(self, user: user): # возможность записи
+        self.db_file = open(self.db_name, "a+")
+        self.db_file.write(user.getInfo()) 
+        self.db_file.write("\n")
+        self.db_file.close()
+
+    def read_all(self): # возможность чтения
+        self.db_file.seek(0)
+        return self.db_file.readlines()
+
+    def searchfor(self, value: str):                
+        with open(self.db_name) as f:
+                for i in f:
+                    if (f"{value}") in i:                       
+                        return True, str(i)
+                                              
+        return False, ""
+
+    
+
+    def update(self, key, value, new_data): # возможность обновления
+        lines = self.read_all()
+        self.db_file.seek(0)
+        self.db_file.truncate(0)
+        for line in lines:
+            if key in line and value in line:
+                line = new_data
+            self.db_file.write(line)
+
+    def delete(self, key, value): # возможность удаления
+        lines = self.read_all()
+        self.db_file.seek(0)
+        self.db_file.truncate(0)
+        for line in lines:
+            if not (key in line and value in line):
+                self.db_file.write(line)
+
+
+    def unload_users(self): # выгрузка пользователей с возможность взаимодействия в цикле
+        a = ""
+        with open(self.db_name, "r") as file:
+            for line in file:
+                a += str(line) 
+
+        return a
+            
+
+
+    def close(self):
+        self.db_file.close()
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-class my_class(object):
-    pass
+import socket
+from Parser import handle_client
 
 
 
+HOST = '127.0.0.1'
+PORT = 65432
 
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
+    conn, addr = server_socket.accept()
+    print(f"server is on {HOST}:{PORT}")   
+    print(f"connected: {addr}")
+
+
+    while True:
+        
+        command, message = handle_client(conn, addr)
+
+
+if __name__ == "__main__":
+    main()
