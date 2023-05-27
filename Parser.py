02064663f75d2187aa0030080d5aa0a36b69ea37
Kim Alexander <kim.aeduard@students.dvfu.ru>
2023-05-27 21:19:10 +1000

    added database, main, user, parser code

---
+++ Parser.py
@@ -0,0 +1,20 @@
+def handle_client(conn, addr):
+    
+    while True:
+        data = conn.recv(1024)
+        message=data.decode('utf-8')
+        if not data:
+            break
+        print(f"got message from {addr}: {message}")
+        command=""
+        for i in message:
+            if i=="?":
+                break
+            else: command+=i
+        return command, f"{message[len(command)+1:]}"
+
+
+    conn.close()
+    print(f"connection from {addr} closed")
+
+
