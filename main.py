6ddf84c00bcb452a9469d29304c11b827d0d01ce
bennnnnja <108936017+bennnnnja@users.noreply.github.com>
2023-05-29 18:13:37 +0300

    Update main.py
    Ким и Юров добавили что-то важное

--- main.py
+++ main.py
@@ -7,45 +7,51 @@ from user import User
 
 HOST = '127.0.0.1'
 PORT = 65432
-database = Database("users.txt")
-
+users = Database("data/users.txt")
+desks = Database("data/desks/desks.txt")
+columns = Database("data/columns/columns.txt")
+cards = Database("data/cards/cards.txt")
+user = "begula:12345678"
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(1)
-    conn, addr = server_socket.accept()
-    print(f"server is on {HOST}:{PORT}")   
-    print(f"connected: {addr}")
-
-
-    while True:
-        
+Expand All
+	@@ -23,27 +27,38 @@ def main():
         command, message = handle_client(conn, addr)
 
         if "NewUser" in command:
-            exists, user = database.searchfor(message)
+            exists, user = users.searchfor(message)
             if not exists:
                 login, password = message.split(":")
                 new_user = User(login, password)
-                database.write(new_user)
-                database.close()
+                users.write(new_user)
+                users.close()
                 conn.send(("Account created!").encode( 'utf-8'))
             else: conn.send(("Account exists!").encode( 'utf-8'))
 
         if "Entrance" in command:
-            accessed, user = database.searchfor(message)
+            accessed, user = users.searchfor(message)
             if accessed:
                 conn.send(("Accepted!").encode('utf-8'))
             else: 
-                
+
                 conn.send(("Declined!").encode('utf-8'))
 
-        if "UnloadUsers" in command:
+        if "NewDesk" in command: 
+            user = "begula:12345678"
+            name, desktype = message.split("!")
+            new_desk = Desk(name, user, desktype)
+            new_desk.create() 
 
-            UnUsers = database.unload_users()
-            conn.send((f"{UnUsers}").encode('utf-8'))
+        if "AddColumn" in command:            
+            desk, column = message.split("!")
+            with open (f"data/desks/{desk}.txt", "a+") as db: 
+                db.write(f"{column}")
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    main()
