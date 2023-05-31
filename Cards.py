2186a6bde87ec8a486425fc016e06f8f42137081
Kim Alexander <kim.aeduard@students.dvfu.ru>
2023-06-01 00:20:17 +1000

    ya ustal

--- Cards.py
+++
@@ -1,8 +0,0 @@
-class Card:
-    def __init__(self, title, author, description, checklist, color, image):
-        self.title = title
-        self.author = author
-        self.description = description
-        self.checklist = checklist
-        self.color = color
-        self.image = image
--- Columns.py
+++
@@ -1,9 +0,0 @@
-class Column:
-    
-    def __init__(self, name, number, cards):
-        self.name = name
-        self.number = number
-        self.cards = cards
-
-    def getInfo(self):
-        return str(f"{self.title}!{self.owner}!{self.columns}!{self.is_public}")
